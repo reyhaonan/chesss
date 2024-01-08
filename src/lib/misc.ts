7f54fa02d02cc4cd09c6ff8db15648b4c1c068dd
reyhaonan <reyhaonan2003@gmail.com>
2024-01-08 15:11:36 +0700

    direction declare

--- src/lib/misc.ts
+++ src/lib/misc.ts
@@ -19,3 +19,18 @@ export enum Piece {
 
 
 export const startingFEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
+
+
+enum Direction {
+  TopLeft = -9,   Top = -8,   TopRight = -7,
+  Left = -1,                  Right = 1,
+  BottomLeft = 7, Bottom = 8, BottomRight = 9,
+}
+
+export const direction = [
+  // Vertical and horizontal
+  Direction.Top, Direction.Right, Direction.Bottom, Direction.Left, 
+
+  // Diagonal
+  Direction.TopLeft, Direction.TopRight, Direction.BottomRight, Direction.BottomLeft
+]
