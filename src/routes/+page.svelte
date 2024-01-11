d3e53b238ded7c51334d8dc1872a48a4020cff2d
reyhaonan <reyhaonan2003@gmail.com>
2024-01-11 17:56:20 +0700

    pre

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,11 +4,7 @@
 	<main class="content col-span-8">
 
 		<div class="w-full flex items-center justify-center my-8">
-			
-			<div class="text-white">
 
-				en passant {enPassantTarget}
-			</div>
 			<Board>
 				<div class="absolute inset-0 grid grid-cols-8 grid-rows-8" id="board">
 					{#each boardArray as pawn, i}
@@ -48,8 +44,8 @@
 	import { convertFENToBoardArray, numberOfTilesToEdge, Piece } from "$lib/method";
 	import { direction, startingFEN, type Move, type Color } from "$lib/misc";
 
-	// let boardArray = convertFENToBoardArray("rnbqkbnr/pppppppp/8/8/pppppppp/8/pppppppp/RNBQKBNR")
-	let boardArray = convertFENToBoardArray(startingFEN)
+	let boardArray = convertFENToBoardArray("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQK2R w KQkq - 0 1")
+	// let boardArray = convertFENToBoardArray(startingFEN)
 
 	let turn:Color = "W";
 
@@ -57,9 +53,20 @@
 
 	let selectedTile:number = -1;
 
-	let castlingRights = ["K","Q","k","q"]
+	let castlingRights = {
+		W:{
+			queenSide: true,
+			kingSide: true
+		},
+		B:{
+			queenSide: true,
+			kingSide: true
+		},
+	}
 
 	let enPassantTarget:number|null = null
+
+	let lastGeneratedMoveList: Move[] =[]
 
 	const generateMoves = () => {
 
@@ -77,7 +84,11 @@
 				else if(Piece.isType(piece, Piece.Knight)){
 					generateKnightMove(i) 
 				}
-				else{
+				else if(Piece.isType(piece, Piece.King)){
+					generateCastlingMove(i, piece)
+					generateSlidingMove(i, piece)
+				}
+				else {
 					generateSlidingMove(i, piece)
 				}
 			}
@@ -92,13 +103,19 @@
 
 		let pieceToMove = boardArray[startTile]
 
+		lastGeneratedMoveList = moveList
+
+		let friendlyColor: Color = pieceToMove < 16? "W":"B"
+
 		let lastEnPassantTarget = enPassantTarget
 
 		enPassantTarget = null;
 
 		boardArray[targetTile] = boardArray[startTile];
 		boardArray[startTile] = Piece.None;
+
 		if(Piece.isType(pieceToMove, Piece.Pawn)){
+			// Set potential en passant target
 			if(Math.abs(targetTile - startTile) === 16)enPassantTarget = targetTile - (targetTile - startTile)/2
 			// Google en passant
 			else if(targetTile === lastEnPassantTarget) {
@@ -114,7 +131,47 @@
 		turn = turn == "B"?"W":"B"
 	}
 
-	const generatePawnMove = (tileIndex:number, piece:number)=>{
+	const generateCastlingMove = (tileIndex:number, piece:number) => {
+		let friendlyColor:Color = piece < 16? "W":"B"
+
+
+		if(castlingRights[friendlyColor].kingSide){
+			let isValid = true;
+
+			// Pre-calculate target tiles for efficiency
+			const targetTiles = [
+				tileIndex + 0,
+				tileIndex + 1,
+				tileIndex + 2,
+				tileIndex + 3
+			];
+
+			// Check for obstacles or attacks in a single loop
+			for (let i = 0; i < 3 && isValid; i++) {
+				isValid = targetTiles[i] === Piece.None && !lastGeneratedMoveList.some(e => e.target === targetTiles[i]);
+			}
+
+
+
+			// Check the rook's position and color
+			isValid = isValid &&
+								Piece.isType(targetTiles[3], Piece.Rook) &&
+								Piece.sameColor(targetTiles[3], friendlyColor);
+
+					console.log("AYAY",targetTiles[2], targetTiles)
+
+			if (isValid) {
+				moveList.push({ start: tileIndex, target: targetTiles[2] });
+			}
+		}else if(castlingRights[friendlyColor].queenSide){
+
+		}
+
+		moveList = moveList
+
+	}
+
+	const generatePawnMove = (tileIndex:number, piece:number) => {
 		let friendlyColor:Color = piece < 16? "W":"B"
 
 		let isOnStartingLine = Piece.sameColor(piece, "W")? Piece.getFile(tileIndex) === 6 : Piece.getFile(tileIndex) === 1
@@ -153,24 +210,24 @@
 	}
 
 	const generateKnightMove = (tileIndex: number) => {
-  const knightMoveOffsets = [
-    -17, -15, -6, 10, 17, 15, 6, -10
-  ];
+		const knightMoveOffsets = [
+			-17, -15, -6, 10, 17, 15, 6, -10
+		];
 
-  for (let i = 0; i < knightMoveOffsets.length; i++) {
-    const targetTile = tileIndex + knightMoveOffsets[i];
+		for (let i = 0; i < knightMoveOffsets.length; i++) {
+			const targetTile = tileIndex + knightMoveOffsets[i];
 
-    // Ensure the target tile is within bounds
-    if (numberOfTilesToEdge[tileIndex][Math.floor(i/2)] >= 2) {
-      const targetPiece = boardArray[targetTile];
+			// Ensure the target tile is within bounds
+			if (numberOfTilesToEdge[tileIndex][Math.floor(i/2)] >= 2) {
+				const targetPiece = boardArray[targetTile];
 
-      // Knight can jump over pieces, so only check for friendly pieces
-      if (!Piece.sameColor(targetPiece, turn)) {
-        moveList.push({ start: tileIndex, target: targetTile });
-      }
-    }
-  }
-};
+				// Knight can jump over pieces, so only check for friendly pieces
+				if (!Piece.sameColor(targetPiece, turn)) {
+					moveList.push({ start: tileIndex, target: targetTile });
+				}
+			}
+		}
+	};
 
 
 	const generateSlidingMove = (tileIndex: number, piece:number) => {
