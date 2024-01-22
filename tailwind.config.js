37ec8b0537515007097d1e2d2d6e6c216ad0f40d
reyhaonan <reyhaonan2003@gmail.com>
2024-01-22 15:06:35 +0700

    feat: roboto condensed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 // import daisyui from "daisyui"
-
+const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,ts,svelte}'],
@@ -25,6 +25,9 @@ export default {
 	},
 	theme: {
 		extend: {
+			fontFamily:{
+				'sans': ['Roboto Condensed', ...defaultTheme.fontFamily.sans]
+			},
 			colors: {
 				// https://www.realtimecolors.com/?colors=ddf1e3-09160d-9fd7b3-335c79-6b7ec1&fonts=Poppins-Poppins
 				textColor: '#ddf1e3',
